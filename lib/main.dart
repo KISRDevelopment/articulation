135a1541bd2047765526f55b1fa2e8f69cffa288
smerri <smerri@kisr.edu.kw>
2023-01-02 12:00:32 +0300

    add letter

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,241 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.lightBlueAccent,
         appBar: AppBar(title: Text("مهارات النطق"),),
-        body: Container(
-          
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+
+                  child: Center(
+                      child: Text("أ", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(color: Colors.yellow,
+                    border: Border.all(
+                        color: Colors.yellow
+
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(20)),),
+
+
+
+
+
+
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+
+              ],
+
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+
+              ],
+
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+
+              ],
+
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+
+              ],
+
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
+                  child: Center(
+                      child: Text("", style: TextStyle(fontSize: 70, ))),
+                  width: 100,
+                  height: 100,
+
+
+
+
+
+                ),
+
+              ],
+
+            ),
+          ],
         ),
       ) ,
     );
