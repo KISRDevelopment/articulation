ad770b5fedea122457df394947e65647b8c611b7
smerri <smerri@kisr.edu.kw>
2023-01-10 11:15:06 +0300

    tap to flashcard

--- lib/screen/singleFlipCard.dart
+++ lib/screen/singleFlipCard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:after_layout/after_layout.dart';
 import 'package:flip_card/flip_card.dart';
+import 'flashCard.dart';
 
 class SingleFlipCard extends StatefulWidget {
   final id;
@@ -13,109 +14,232 @@ class SingleFlipCard extends StatefulWidget {
 }
 
 class _SingleFlipCardState extends State<SingleFlipCard> with AfterLayoutMixin<SingleFlipCard> {
+  bool flag = false;
   final GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Center(
-        child: Container(
-          color: Colors.transparent,
-          height: 700,
+    return GestureDetector(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
           child: Container(
-            //color: Colors.white,
-            width: 700,
+            color: Colors.transparent,
             height: 700,
-            decoration: BoxDecoration(
-                color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(20)
-            ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(360)
-                    ),
-                  ),
-                  child: Icon(Icons.lightbulb_outline, size: 50),
-
-                ),
-                Container(
-                  child: Center(child: Text('أ', style: TextStyle(fontSize: 150, ),)),
-                ),
-                Container(
-                  child: Row(
+            child: Container(
+              //color: Colors.white,
+              width: 700,
+              height: 700,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20)
+              ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        width: 150,
-                        height: 100,
-                        color: Colors.yellow,
-                        child: Column(
-                          children: [
-                            Text("قصص", style: TextStyle(fontSize: 50)),
-                            //Icon(Icons.lightbulb_outline, size: 50)
+                      AnimatedCrossFade(
+                        firstChild: Container(
+                          margin: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(360)
+                            ),
+                          ),
+                          //child: Icon(Icons.lightbulb_outline, size: 50),
 
-                          ],
                         ),
+                        secondChild: GestureDetector(
+                          child: Container(
+                            margin: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.all(Radius.circular(360)
+                              ),
+                            ),
+                            child: Icon(Icons.arrow_back_ios, size: 50),
+
+                          ),
+                          onTap: (){
+                            setState(() {
+                              flag = !flag;
+                            });
+                          },
+                        ),
+                        crossFadeState: flag ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+                        duration: Duration(milliseconds: 500),
                       ),
                       Container(
-                        margin: EdgeInsets.all(10),
-                        width: 150,
-                        height: 100,
-                        color: Colors.yellow,
-                        child: Column(
-                          children: [
-                            Text("جمل", style: TextStyle(fontSize: 50)),
-                            //Icon(Icons.lightbulb_outline, size: 50)
-
-                          ],
+                        margin: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(360)
+                          ),
                         ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        width: 150,
-                        height: 100,
-                        color: Colors.yellow,
-                        child: Column(
-                          children: [
-                            Text("عبارات", style: TextStyle(fontSize: 50)),
-                            //Icon(Icons.lightbulb_outline, size: 50)
+                        child: Icon(Icons.lightbulb_outline, size: 50),
 
-                          ],
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        width: 150,
-                        height: 100,
-                        color: Colors.yellow,
-                        child: Column(
-                          children: [
-                            Text("كلمات" , style: TextStyle(fontSize: 50),),
-                           // Icon(Icons.lightbulb_outline, size: 50)
-
-                          ],
-                        ),
                       ),
                     ],
                   ),
-                ),
-              ],
+                  Container(
+                    child: Center(child: Text('أ', style: TextStyle(fontSize: 150, ),)),
+                  ),
+                  Container(
+                    child: AnimatedCrossFade(
+                      firstChild: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            width: 150,
+                            height: 100,
+                            color: Colors.yellow,
+                            child: Column(
+                              children: [
+                                Text("قصص", style: TextStyle(fontSize: 50)),
+                                //Icon(Icons.lightbulb_outline, size: 50)
 
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            width: 150,
+                            height: 100,
+                            color: Colors.yellow,
+                            child: Column(
+                              children: [
+                                Text("جمل", style: TextStyle(fontSize: 50)),
+                                //Icon(Icons.lightbulb_outline, size: 50)
+
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            width: 150,
+                            height: 100,
+                            color: Colors.yellow,
+                            child: Column(
+                              children: [
+                                Text("عبارات", style: TextStyle(fontSize: 50)),
+                                //Icon(Icons.lightbulb_outline, size: 50)
+
+                              ],
+                            ),
+                          ),
+                          GestureDetector(
+                            child: Container(
+                              margin: EdgeInsets.all(10),
+                              width: 150,
+                              height: 100,
+                              color: Colors.yellow,
+                              child: Column(
+                                children: [
+                                  Text("كلمات" , style: TextStyle(fontSize: 50),),
+                                 // Icon(Icons.lightbulb_outline, size: 50)
+
+                                ],
+                              ),
+                            ),
+                            onTap: (){
+                              setState(() {
+                                flag = !flag;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      secondChild: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            width: 150,
+                            height: 100,
+                            color: Colors.white,
+                            child: Column(
+                              children: [
+                                Text("", style: TextStyle(fontSize: 50)),
+                                //Icon(Icons.lightbulb_outline, size: 50)
+
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            width: 150,
+                            height: 100,
+                            color: Colors.yellow,
+                            child: Column(
+                              children: [
+                                Text("العاب ", style: TextStyle(fontSize: 50)),
+                                //Icon(Icons.lightbulb_outline, size: 50)
+
+                              ],
+                            ),
+                          ),
+                          GestureDetector(
+                            child: Container(
+                              margin: EdgeInsets.all(10),
+                              width: 150,
+                              height: 100,
+                              color: Colors.yellow,
+                              child: Column(
+                                children: [
+                                  Text("بطاقات", style: TextStyle(fontSize: 50)),
+                                  //Icon(Icons.lightbulb_outline, size: 50)
+
+                                ],
+                              ),
+                            ),
+                              onTap: (){
+                                Navigator.of(context).push(
+                                    PageRouteBuilder(
+                                      pageBuilder: (_, __, ___) => flashCard(),
+                                    )
+                                );
+                              }
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            width: 150,
+                            height: 100,
+                            color: Colors.white,
+                            child: Column(
+                              children: [
+                                Text("" , style: TextStyle(fontSize: 50),),
+                                // Icon(Icons.lightbulb_outline, size: 50)
+
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      crossFadeState: flag ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+                      duration: Duration(milliseconds: 500),
+                    ),
+                  ),
+                ],
+
+              ),
             ),
+
+
           ),
-
-
         ),
       ),
+      onTap: (){
+        Navigator.of(context).pop(
+        );
+      },
     );
   }
 
