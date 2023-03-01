d9c6fb5cec953f663adfddd95994ec3485fa5987
smerri <smerri@kisr.edu.kw>
2023-03-01 11:39:36 +0300

    add class for images

---
+++ lib/screen/options.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class PicturesOptions{
+  final String word;
+  final String image;
+
+
+
+
+
+
+
+
+
+
+  PicturesOptions({
+    required this.word,
+    required this.image,
+  });
+
+  List<Object?> get pic => [word,image];
+
+
+
+
+
+
+
+
+  static List<PicturesOptions> picListBeginAlf = [
+    PicturesOptions(word: "أسد", image: "images/lion2.jpg" ),
+  ];
+
+  static List<PicturesOptions> picListBeginBaa = [
+    PicturesOptions(word: "بطة", image: "images/duck2.jpg"),
+  ];
+
+  static List<PicturesOptions> picListBeginTaa = [
+    PicturesOptions(word: "تفاحة", image: "images/apple.jpg"),
+  ];
+
+  static List<PicturesOptions> picListBeginTh = [
+    PicturesOptions(word: "ثعلب", image: "images/fox.jpg"),
+  ];
+
+
+  static List<PicturesOptions> picListMedAlf = [
+    PicturesOptions(word: "فأر", image: "images/mouse2.jpg"),
+  ];
+
+  static List<PicturesOptions> picListMedBaa = [
+    PicturesOptions(word: "خبز", image: "images/bread.jpg"),
+  ];
+
+  static List<PicturesOptions> picListMedTaa = [
+    PicturesOptions(word: "دفتر", image: "images/notebook.jpg"),
+  ];
+
+  static List<PicturesOptions> picListMedTh = [
+    PicturesOptions(word: "كمثري", image: "images/pear.jpg"),
+  ];
+
+
+  static List<PicturesOptions> picListEndAlf = [
+    PicturesOptions(word: "عصا", image: "images/stick.jpg"),
+  ];
+
+  static List<PicturesOptions> picListEndBaa = [
+    PicturesOptions(word: "كلب", image: "images/dog.jpg"),
+  ];
+
+  static List<PicturesOptions> picListEndTaa = [
+    PicturesOptions(word: "زيت؛", image: "images/oil.jpg"),
+  ];
+
+  static List<PicturesOptions> picListEndTh = [
+    PicturesOptions(word: "مثلث", image: "images/traingle.jpg"),
+  ];
+
+
+
+
+
+
+
+
+
+
+
+}
