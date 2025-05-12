9d6ee26506b8d06ea363b4ae341efe544a477192
smerri <smerri@kisr.edu.kw>
2025-05-12 14:24:21 +0300

    readable file

--- lib/screen/flashcardWord.dart
+++ lib/screen/flashcardWord.dart
@@ -1,22 +1,22 @@
-import 'package:articulation/screen/options.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/material.dart' hide CarouselController;
-import 'singleFlipCard.dart';
+import 'package:flutter/material.dart';
+//import 'flashCardOption.dart';
+import 'word.dart';
 import 'options.dart';
 
-
 class flashCardWord extends StatefulWidget {
-
-
-  final dict;
-  const flashCardWord(@required this.dict);
+  final letter;
+  const flashCardWord(@required this.letter);
 
   @override
-  _flashCardState createState() => _flashCardState();
+  _flashCardWordState createState() => _flashCardWordState();
 }
 
-class _flashCardState extends State<flashCardWord> {
+class _flashCardWordState extends State<flashCardWord> {
+  late List dict;
   bool flag = false;
+  bool medial = false;
+  bool end = false;
+  late String letters;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,24 +52,187 @@ class _flashCardState extends State<flashCardWord> {
                   borderRadius: BorderRadius.all(Radius.circular(20)
                   ),
                 ),
-                child: CarouselSlider(
-                  items: widget.dict.map<Widget>((pictureOptions) =>
-                  CarouselCard(picturesOptions: pictureOptions)).toList(),
-                  options: CarouselOptions(
-                    height: MediaQuery.of(context).size.height,
-                    viewportFraction: 1.0,
-                    enlargeCenterPage: true,
-                    onPageChanged: (position,reason){
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Center(
+                        child: Container(
+                          margin: EdgeInsets.all(10),
+                          child: Text(widget.letter, style: TextStyle(fontSize: 70),),
 
-                    },
-                    enableInfiniteScroll: false,
-                  ),
+                        )),
+                    Container(
+                      height: 100,
+                      // color: Colors.grey[300],
+                      margin: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+
+                          GestureDetector(
+                            child: Container(
+                              height: 100,
+                              width: 250,
+                              child: Center(child: Text("بداية الكلمة", style: TextStyle(fontSize: 50, color: flag ? Colors.white : Colors.grey), )),
+                              decoration: BoxDecoration(
+                                color: flag ? Colors.grey : Colors.grey[100],
+                                borderRadius: BorderRadius.all(Radius.circular(20)
+                                ),
+                              ),
+
+                            ),
+                            onTap: (){
+                              setState(() {
+
+                                flag = !flag;
+
+                                if(medial == true){
+                                  medial = !medial;
+                                }else if(end == true){
+                                  end = !end;
+                                }
+                                letters = widget.letter;
+                                dict = showPosition('begin');
 
 
+
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      //color: Colors.grey[300],
+                      margin: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            child: Container(
+                              height: 100,
+                              width: 250,
+                              child: Center(child: Text("وسط الكلمة", style: TextStyle(fontSize: 50, color: medial ? Colors.white : Colors.grey))),
+                              decoration: BoxDecoration(
+                                color: medial ? Colors.grey : Colors.grey[100],
+                                borderRadius: BorderRadius.all(Radius.circular(20)
+                                ),
+                              ),),
+                            onTap: (){
+                              setState(() {
+                                medial = !medial;
+
+                                if(flag == true){
+                                  flag = !flag;
+                                }else if(end == true){
+                                  end = !end;
+                                }
+                                letters = widget.letter;
+                                dict = showPosition('medial');
+
+
+
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      // color: Colors.grey[300],
+                      margin: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            child: Container(
+                              height: 100,
+                              width: 250,
+                              child: Center(child: Text("آخر الكلمة", style: TextStyle(fontSize: 50, color: end ? Colors.white : Colors.grey))),
+                              decoration: BoxDecoration(
+                                color: end ? Colors.grey : Colors.grey[100],
+                                borderRadius: BorderRadius.all(Radius.circular(20)
+                                ),
+                              ),),
+                            onTap: (){
+                              setState(() {
+                                end = !end;
+
+                                if(medial == true){
+                                  medial = !medial;
+                                }else if(flag == true){
+                                  flag = !flag;
+                                }
+                                letters = widget.letter;
+
+                                dict = showPosition('end');
+
+
+
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
+            AnimatedCrossFade(
+              firstChild: Container(
+                margin: EdgeInsets.only(top: 250, left: 950, right: 10, bottom: 10),
+                width: 70,
+                height: 70,
 
+
+              ),
+              secondChild: Container(
+                margin: EdgeInsets.only(top: 250, left: 950, right: 10, bottom: 10),
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.all(Radius.circular(50)
+                  ),
+                ),
+                child: GestureDetector(child: Center(
+                    child: Text("ابدأ", style: TextStyle(
+                        color: Colors.black, fontSize: 35)
+                    )
+                ),
+                  onTap:(){
+                    //dict = showPosition('begin');
+                    Navigator.of(context).push(
+                        PageRouteBuilder(
+                          pageBuilder: (_, __, ___) => word(dict),
+                        )
+                    );
+                  }
+                ),
+              ),
+              crossFadeState: (flag||medial||end) ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+              duration: Duration(milliseconds: 500),
+            ),
+            AnimatedCrossFade(
+              firstChild: Container(
+                margin: EdgeInsets.only(top: 350, left: 950, right: 10, bottom: 10),
+                width: 70,
+                height: 70,
+              ),
+                secondChild: Container(
+                  margin: EdgeInsets.only(top: 350, left: 950, right: 10, bottom: 10),
+                  width: 70,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlueAccent,
+                    borderRadius: BorderRadius.all(Radius.circular(360)
+                    ),
+                  ),
+                  child: Icon(Icons.view_list, size: 50.0),
+                ),
+              crossFadeState: (flag||medial||end) ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+              duration: Duration(milliseconds: 500),
+            ),
             Container(
               margin: EdgeInsets.only(top: 750, left: 1000, right: 10, bottom: 10),
               width: 50,
@@ -84,45 +247,306 @@ class _flashCardState extends State<flashCardWord> {
           ],
         ),
       ),
-    );
+      );
     //);
   }
-}
-
-class CarouselCard extends StatelessWidget {
-  final PicturesOptions picturesOptions;
-
-  const CarouselCard({required this.picturesOptions});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(5.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.all(Radius.circular(5.0)),
-        child: Stack(
-          children: [
-            Container(
-
-              padding: EdgeInsets.fromLTRB(0, 150, 0, 100),
-                child: Image.asset(picturesOptions.image, fit: BoxFit.cover)),
-            Positioned(
-              right: 0.0,
-                bottom: 0.0,
-                left: 0.0,
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 10.0),
-                  child: Center(
-                    child: Text(picturesOptions.word,
-                    style: TextStyle(color: Colors.black,
-                    fontSize: 50.0,
-                    fontWeight: FontWeight.bold),),
-                  ),
-                ))
-          ],
-        ),
-      ),
-    );
+  showPosition(pos){
+    switch(pos){
+      case 'begin':
+        dict = showLetterBegin(letters);
+        return dict;
+      case 'medial':
+        dict = showLetterMed(letters);
+        return dict;
+      case 'end':
+        dict = showLetterEnd(letters);
+        return dict;
+      default:
+        break;
+    }
   }
-}
+  showLetterBegin(pos){
+    switch(pos){
+      case 'أ':
+        dict = PicturesOptions.picListBeginAlf;
+        return dict;
+      case 'ث':
+        dict = PicturesOptions.picListBeginTh;
+        return dict;
+      case 'ت':
+        dict = PicturesOptions.picListBeginTaa;
+        return dict;
+      case 'ب':
+        dict = PicturesOptions.picListBeginBaa;
+        return dict;
+      case 'ج':
+        dict = PicturesOptions.picListBeginJa;
+        return dict;
+      case 'ح':
+        dict = PicturesOptions.picListBeginHa;
+        return dict;
+      case 'خ':
+        dict = PicturesOptions.picListBeginKha;
+        return dict;
+      case 'د':
+        dict = PicturesOptions.picListBeginDa;
+        return dict;
+      case 'ذ':
+        dict = PicturesOptions.picListBeginTha;
+        return dict;
+      case 'ر':
+        dict = PicturesOptions.picListBeginRa;
+        return dict;
+      case 'ز':
+        dict = PicturesOptions.picListBeginZa;
+        return dict;
+      case 'س':
+        dict = PicturesOptions.picListBeginSa;
+        return dict;
+      case 'ش':
+        dict = PicturesOptions.picListBeginSha;
+        return dict;
+      case 'ص':
+        dict = PicturesOptions.picListBeginSsa;
+        return dict;
+      case 'ض':
+        dict = PicturesOptions.picListBeginTtha;
+        return dict;
+      case 'ط':
+        dict = PicturesOptions.picListBeginTta;
+        return dict;
+      case 'ظ':
+        dict = PicturesOptions.picListBeginZha;
+        return dict;
+      case 'ع':
+        dict = PicturesOptions.picListBeginAa;
+        return dict;
+      case 'غ':
+        dict = PicturesOptions.picListBeginGha;
+        return dict;
+      case 'ف':
+        dict = PicturesOptions.picListBeginFa;
+        return dict;
+      case 'ق':
+        dict = PicturesOptions.picListBeginQa;
+        return dict;
+      case 'ك':
+        dict = PicturesOptions.picListBeginKa;
+        return dict;
+      case 'ل':
+        dict = PicturesOptions.picListBeginLa;
+        return dict;
+      case 'م':
+        dict = PicturesOptions.picListBeginMa;
+        return dict;
+      case 'ن':
+        dict = PicturesOptions.picListBeginNa;
+        return dict;
+      case 'هـ':
+        dict = PicturesOptions.picListBeginHuh;
+        return dict;
+      case 'و':
+        dict = PicturesOptions.picListBeginWa;
+        return dict;
+      case 'ي':
+        dict = PicturesOptions.picListBeginYa;
+        return dict;
 
+
+
+
+
+
+      default:
+        break;
+    }
+  }
+  showLetterMed(pos){
+    switch(pos){
+      case 'أ':
+        dict = PicturesOptions.picListMedAlf;
+        return dict;
+      case 'ث':
+        dict = PicturesOptions.picListMedTh;
+        return dict;
+      case 'ت':
+        dict = PicturesOptions.picListMedTaa;
+        return dict;
+      case 'ب':
+        dict = PicturesOptions.picListMedBaa;
+        return dict;
+      case 'ج':
+        dict = PicturesOptions.picListMedJa;
+        return dict;
+      case 'ح':
+        dict = PicturesOptions.picListMedHa;
+        return dict;
+      case 'خ':
+        dict = PicturesOptions.picListMedKha;
+        return dict;
+      case 'د':
+        dict = PicturesOptions.picListMedDa;
+        return dict;
+      case 'ذ':
+        dict = PicturesOptions.picListMedTha;
+        return dict;
+      case 'ر':
+        dict = PicturesOptions.picListMedRa;
+        return dict;
+      case 'ز':
+        dict = PicturesOptions.picListMedZa;
+        return dict;
+      case 'س':
+        dict = PicturesOptions.picListMedSa;
+        return dict;
+      case 'ش':
+        dict = PicturesOptions.picListMedSha;
+        return dict;
+      case 'ص':
+        dict = PicturesOptions.picListMedSsa;
+        return dict;
+      case 'ض':
+        dict = PicturesOptions.picListMedTtha;
+        return dict;
+      case 'ط':
+        dict = PicturesOptions.picListMedTta;
+        return dict;
+      case 'ظ':
+        dict = PicturesOptions.picListMedZha;
+        return dict;
+
+      case 'ع':
+        dict = PicturesOptions.picListMedAa;
+        return dict;
+      case 'غ':
+        dict = PicturesOptions.picListMedGha;
+        return dict;
+      case 'ف':
+        dict = PicturesOptions.picListMedFa;
+        return dict;
+      case 'ق':
+        dict = PicturesOptions.picListMedQa;
+        return dict;
+      case 'ك':
+        dict = PicturesOptions.picListMedKa;
+        return dict;
+      case 'ل':
+        dict = PicturesOptions.picListMedLa;
+        return dict;
+      case 'م':
+        dict = PicturesOptions.picListMedMa;
+        return dict;
+      case 'ن':
+        dict = PicturesOptions.picListMedNa;
+        return dict;
+      case 'هـ':
+        dict = PicturesOptions.picListMedHuh;
+        return dict;
+      case 'و':
+        dict = PicturesOptions.picListMedWa;
+        return dict;
+      case 'ي':
+        dict = PicturesOptions.picListMedYa;
+        return dict;
+      default:
+        break;
+    }
+  }
+  showLetterEnd(pos){
+    switch(pos){
+      case 'أ':
+        dict = PicturesOptions.picListEndAlf;
+        return dict;
+      case 'ث':
+        dict = PicturesOptions.picListEndTh;
+        return dict;
+      case 'ت':
+        dict = PicturesOptions.picListEndTaa;
+        return dict;
+      case 'ب':
+        dict = PicturesOptions.picListEndBaa;
+        return dict;
+      case 'ج':
+        dict = PicturesOptions.picListEndJa;
+        return dict;
+      case 'ح':
+        dict = PicturesOptions.picListEndHa;
+        return dict;
+      case 'خ':
+        dict = PicturesOptions.picListEndKha;
+        return dict;
+      case 'د':
+        dict = PicturesOptions.picListEndDa;
+        return dict;
+      case 'ذ':
+        dict = PicturesOptions.picListEndTha;
+        return dict;
+      case 'ر':
+        dict = PicturesOptions.picListEndRa;
+        return dict;
+      case 'ز':
+        dict = PicturesOptions.picListEndZa;
+        return dict;
+      case 'س':
+        dict = PicturesOptions.picListEndSa;
+        return dict;
+      case 'ش':
+        dict = PicturesOptions.picListEndSha;
+        return dict;
+      case 'ص':
+        dict = PicturesOptions.picListEndSsa;
+        return dict;
+      case 'ض':
+        dict = PicturesOptions.picListEndTtha;
+        return dict;
+      case 'ط':
+        dict = PicturesOptions.picListEndTta;
+        return dict;
+      case 'ظ':
+        dict = PicturesOptions.picListEndZha;
+        return dict;
+      case 'ع':
+        dict = PicturesOptions.picListEndAa;
+        return dict;
+      case 'غ':
+        dict = PicturesOptions.picListEndGha;
+        return dict;
+      case 'ف':
+        dict = PicturesOptions.picListEndFa;
+        return dict;
+      case 'ق':
+        dict = PicturesOptions.picListEndQa;
+        return dict;
+      case 'ك':
+        dict = PicturesOptions.picListEndKa;
+        return dict;
+      case 'ل':
+        dict = PicturesOptions.picListEndLa;
+        return dict;
+      case 'م':
+        dict = PicturesOptions.picListEndMa;
+        return dict;
+      case 'ن':
+        dict = PicturesOptions.picListEndNa;
+        return dict;
+      case 'هـ':
+        dict = PicturesOptions.picListEndHuh;
+        return dict;
+      case 'و':
+        dict = PicturesOptions.picListEndWa;
+        return dict;
+      case 'ي':
+        dict = PicturesOptions.picListEndYa;
+        return dict;
+
+
+
+      default:
+        break;
+    }
+  }
+
+
+
+
+}
