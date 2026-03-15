8a11244d8bb9a7a58a7c60a0cc153470a4fe0698
smerri <smerri@kisr.edu.kw>
2026-03-15 17:07:50 +0300

    direct the setting button to setting page

--- lib/main.dart
+++ lib/main.dart
@@ -838,7 +838,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     onTap: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => SettingPage()),
+                          MaterialPageRoute(builder: (context) => SettingsPage(patientId: civilID)),
                         );
                       }
                   ),
